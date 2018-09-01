5280fef46a9a4896f00b16ef633a4606634f59d4
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-09-01 13:09:00 +0530

    Adding Directory

---
+++ CSPP1-Assignment/m2/Assignment-2/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static float rootsOfQuadraticEquation(){
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		print(rootsOfQuadraticEquation(a, b, c, "+"));
+		print(rootsOfQuadraticEquation(a, b, c, "-"));
+	}
+	/*
+	Need to write the rootsOfQuadraticEquation function and print the output.
+	*/
+}
