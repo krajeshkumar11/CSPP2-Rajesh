24e42f09b9bf88c0b7a9abb2078b0f9b3565b197
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-09-03 15:38:35 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- CSPP1-Assignment/m2/Assignment-3/Solution.java
+++ CSPP1-Assignment/m2/Assignment-3/Solution.java
@@ -4,8 +4,16 @@ public class Solution {
 	/*
 	Do not modify this main function.
 	*/
+    public static int power(int base, int expo){
+        int sum = 1;
+        while(expo > 0){
+            sum = sum * base;
+            expo -= 1;
+        }
+        return sum;
+    }
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+		Scanner s=new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         int result=power(base,exponent);
