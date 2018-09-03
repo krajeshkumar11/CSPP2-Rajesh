11cdcb7f211ae47c433011197e99a208f763e7a0
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-09-03 15:33:17 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP1-Assignment/m2/Assignment-2/Solution.java
+++ CSPP1-Assignment/m2/Assignment-2/Solution.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
 import java.lang.Math;
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
+	/**
+	 * { function_description }
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      b     { parameter_description }
+	 * @param      c     { parameter_description }
+	 * @param      si    { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public static double rootsOfQuadraticEquation(int a, int b,int c, char si){
 		if(si == '+'){
 			return (-b + Math.sqrt(Math.pow(b,2) - (4 * a * c)))/(2 * a);
@@ -11,6 +18,11 @@ public class Solution {
 			return (-b - Math.sqrt(Math.pow(b,2) - (4 * a * c)))/(2 * a);
 		}
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
