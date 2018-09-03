d76b2d251b31696ac4f412bf3e421a2eebef949b
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-09-03 16:18:18 +0530

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- CSPP1-Assignment/m3/Assignment-3/Solution.java
+++ CSPP1-Assignment/m3/Assignment-3/Solution.java
@@ -5,9 +5,23 @@ import java.util.Scanner;
 	*/
 public class Solution {
 
+    public static int gcd = 0;
+    public static void gcd(int n1, int n2) {
+        int max = 0;
+        if(n1 > n2){
+            max = n1;
+        }else{
+            max = n2;
+        }
+        for(int i = 1; i< max ; i++){
+            if(n1 % i == 0 && n2 % 2 == 0 ){
+                gcd = i;
+            }
+        }
+    }
     public static void main(String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s=new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1,n2);
