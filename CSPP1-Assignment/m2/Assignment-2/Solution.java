cffa057788413d54f387eda3a45629157c3d86cf
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-09-03 15:27:17 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- CSPP1-Assignment/m2/Assignment-2/Solution.java
+++ CSPP1-Assignment/m2/Assignment-2/Solution.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
-
+import java.lang.Math;
 public class Solution {
 	/*
 	Do not modify this main function.
 	*/
-	public static float rootsOfQuadraticEquation(){
-
+	public static double rootsOfQuadraticEquation(int a, int b,int c, char si){
+		if(si == '+'){
+			return (-(b) + Math.sqrt((Math.pow(b,2)) - 4 * a * c))/2 * a;
+		}else{
+			return (-(b) - Math.sqrt((Math.pow(b,2)) - 4 * a * c))/2 * a;
+		}
 	}
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		print(rootsOfQuadraticEquation(a, b, c, "+"));
-		print(rootsOfQuadraticEquation(a, b, c, "-"));
+		System.out.print(rootsOfQuadraticEquation(a, b, c, '+'));
+		System.out.print(" ");
+		System.out.print(rootsOfQuadraticEquation(a, b, c, '-'));
 	}
 	/*
 	Need to write the rootsOfQuadraticEquation function and print the output.
