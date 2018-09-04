7c60f1ddea52229b7606a63d14ac30c56e835926
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-09-04 15:31:42 +0530

    Adding m8.

---
+++ CSPP1-Assignment/m8/CodecampList/ListInterface.java
@@ -0,0 +1,13 @@
+interface ListInterface{
+    public void add(int item);
+    public int size();
+    public void remove(int index);
+    public int get(int index);
+    public String toString();
+    public boolean contains(int item);
+    public int indexOf(int item);
+
+    public void addAll(int[] items);
+    public void add(int index, int item);
+    public int count(int item);
+}
