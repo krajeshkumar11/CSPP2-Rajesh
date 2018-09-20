4f727dfa9c32678b66f1f714f762bc434a4693e1
krajeshkumar11 <k.rajeshkumar@msitprogram.net>
2018-09-20 15:08:52 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- CSPP1-Assignment/m23/Assignment-1/BOW.java
+++ CSPP1-Assignment/m23/Assignment-1/BOW.java
@@ -4,33 +4,64 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.lang.Math;
 
+/**
+ * Class for document.
+ */
 class Document {
 	private String document;
 	private HashMap<String, Integer> hashMap;
 	private double euclideanNormValue;
 
+	/**
+	 * Constructs the object.
+	 */
 	public Document () {
 		this.document = "";
 		this.hashMap = new HashMap<String, Integer>();
 		this.euclideanNormValue = 0;
 	}
 
+	/**
+	 * Gets the document.
+	 *
+	 * @return     The document.
+	 */
 	public String getDocument() {
 		return this.document;
 	}
 
+	/**
+	 * Gets the hash map.
+	 *
+	 * @return     The hash map.
+	 */
 	public HashMap<String, Integer> getHashMap() {
 		return this.hashMap;
 	}
 
+	/**
+	 * Gets the euclidean normalize value.
+	 *
+	 * @return     The euclidean normalize value.
+	 */
 	public double getEuclideanNormValue () {
 		return euclideanNormValue;
 	}
 
+	/**
+	 * { function_description }
+	 */
 	public void cleanString() {
 		this.document = this.document.replaceAll("[0-9_]","").toLowerCase();
 	}
 
+	/**
+	 * Reads a file.
+	 *
+	 * @param      f1         The f 1
+	 *
+	 * @throws     Exception  { exception_description }
+	 */
 	public void readFile(File f1)throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(f1));
         String data = "";
@@ -41,6 +72,9 @@ class Document {
         this.document = data;
     }
 
+    /**
+     * { function_description }
+     */
     public void generateHashMap() {
         HashMap<String, Integer> HM = new HashMap<>();
         String[] stArr = this.document.split(" ");
@@ -55,6 +89,9 @@ class Document {
         this.hashMap = HM;
     }
 
+    /**
+     * { function_description }
+     */
     public void generateEuclideanNorm () {
         Integer[] hmvals = this.hashMap.values().toArray(new Integer[0]);
         double total = 0;
@@ -67,8 +104,19 @@ class Document {
     }
 }
 
+/**
+ * Class for bow.
+ */
 public class BOW {
 
+    /**
+     * Gets the dot product.
+     *
+     * @param      hm1   The hm 1
+     * @param      hm2   The hm 2
+     *
+     * @return     The dot product.
+     */
     public static HashMap<String, Integer> getDotProduct(HashMap<String, Integer> hm1, HashMap<String, Integer> hm2) {
         HashMap<String, Integer> dp = new HashMap<String, Integer>();
         String[] f1 = hm1.keySet().toArray(new String[0]);
@@ -83,6 +131,13 @@ public class BOW {
         return dp;
     }
 
+    /**
+     * Gets the sum of dp.
+     *
+     * @param      hm    { parameter_description }
+     *
+     * @return     The sum of dp.
+     */
     public static double getSumOfDP(HashMap<String, Integer> hm) {
         Integer[] hmvals = hm.values().toArray(new Integer[0]);
         int total = 0;
@@ -92,6 +147,13 @@ public class BOW {
         return total;
     }
 
+    /**
+     * { function_description }
+     *
+     * @param      args       The arguments
+     *
+     * @throws     Exception  { exception_description }
+     */
     public static void main(String[] args)throws Exception {
         Scanner sc = new Scanner(System.in);
         String st = "";
